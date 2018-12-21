48016cc663b15263520c10ecc278e2a0dd201e4c
ryanfuchs <44633422+ryanfuchs@users.noreply.github.com>
2018-12-21 14:14:53 +0100

    added Menu

--- ListenSortierung/ListenSortierung/ListenSortierung.cpp
+++ ListenSortierung/ListenSortierung/ListenSortierung.cpp
@@ -3,9 +3,44 @@
 #include <math.h>
 
 char getRandomCharacter();
+void printMenu(bool AnyAvaiableList);
 
 int main() {
-	system("pause");
+	bool AnyAvaiableList = false;
+
+	printMenu(AnyAvaiableList);
+	
+}
+
+void printMenu(bool AnyAvaiableList) {
+	if (AnyAvaiableList == false) {
+		printf("******************************");
+		printf("\n\tVerkette Liste");
+		printf("\n******************************");
+
+		printf("\n\n\n[1] Liste erstellen");
+		printf("\n[2] Liste loeschen ");
+		printf("\n[3] Person loeschen");
+		printf("\n[4] Liste sortieren");
+		printf("\n[5] Liste ausgeben");
+		printf("\n[6] Programm beenden\n");
+		system("pause");
+	}
+
+	else
+	{
+		printf("******************************");
+		printf("\n\tVerkette Liste");
+		printf("\n******************************");
+
+		printf("\n\n\n[1] Liste erstellen");
+		printf("\n[2] Liste loeschen ");
+		printf("\n[3] Person loeschen");
+		printf("\n[4] Liste sortieren");
+		printf("\n[5] Liste ausgeben");
+		printf("\n[6] Programm beenden\n");
+		system("pause");
+	}
 }
 
 char getRandomCharacter() {
